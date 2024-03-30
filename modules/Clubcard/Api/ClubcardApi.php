71cb88812715cdc57c74c3ab39459a534bf238a8
k3rielit <ahurkatolto@gmail.com>
2024-03-30 17:19:52 +0100

    Added halfway done clubcard country code support, headers assembler

--- modules/Clubcard/Api/ClubcardApi.php
+++ modules/Clubcard/Api/ClubcardApi.php
@@ -3,6 +3,7 @@
 namespace Modules\Clubcard\Api;
 
 use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
 use Illuminate\Support\Collection;
 
 class ClubcardApi
@@ -11,12 +12,12 @@ class ClubcardApi
     protected Collection $countryCodes;
     protected Guzzle $client;
 
-    public function __construct()
+    public function __construct(?string $baseUri = null)
     {
         $this->userAgents = collect(config('clubcard.user_agents'));
         $this->countryCodes = collect(config('clubcard.country_codes'));
         $this->client = new Guzzle([
-            'base_uri' => config('clubcard.base_uri'),
+            'base_uri' => $baseUri ?? config('clubcard.base_uri'),
         ]);
     }
 
@@ -54,7 +55,56 @@ class ClubcardApi
         }
     }
 
-    // Requests
+    // Country code
 
+    public function countryCode(string $countryCode): static
+    {
+        $this->countryCodes = collect([$countryCode]);
+        return $this;
+    }
+
+    public function countryCodes(Collection|array $countryCodes): static
+    {
+        $this->countryCodes = is_array($countryCodes) ? collect($countryCodes) : $countryCodes;
+        return $this;
+    }
+
+    public function pushCountryCodes(Collection|array $countryCodes): static
+    {
+        $this->countryCodes->push(...$countryCodes);
+        return $this;
+    }
+
+    public function getCountryCodes(): Collection
+    {
+        return $this->countryCodes;
+    }
+
+    public function getCountryCode(): ?string
+    {
+        try {
+            return $this->countryCodes->random();
+        } catch (\Throwable $exception) {
+            return null;
+        }
+    }
+
+    // Headers
+
+    public function getHeaders(): array
+    {
+        return [
+            'User-Agent' => $this->getUserAgent(),
+            'Country-Code' => $this->getCountryCode(),
+        ];
+    }
+
+    // Requests - Registration
+
+    public function registrationIghsCheck(): string
+    {
+        $request = new GuzzleRequest('POST', '/v3/registrations/ighs/check');
+        return '';
+    }
 
 }
