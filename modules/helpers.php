b8290b328feb5e5fce2f04c21e1c8199c3544e14
k3rielit <ahurkatolto@gmail.com>
2024-03-30 16:23:01 +0100

    Added module_path helper

--- modules/helpers.php
+++ modules/helpers.php
@@ -1,1 +1,14 @@
 <?php
+
+if (!function_exists('module_path')) {
+    /**
+     * Get the path to the modules directory.
+     *
+     * @param string $path
+     * @return string
+     */
+    function module_path(string $path = ''): string
+    {
+        return app()->basePath("modules/{$path}");
+    }
+}
